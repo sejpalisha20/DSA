fff8324e65305970a0d91ed2f97acb3d26a52439
Isha Sejpal <sejpalisha20@gmail.com>
2026-09-22 21:34:58 +0530

    Remove commented-out code and clean up print statements

--- variables.py
+++ variables.py
@@ -1,34 +1,34 @@
 #variables
-"""name = "Isha"
+name = "Isha"
 age = 20
 city = "Rajkot"
 print("Name:", name)
 print("Age:", age)
-print("City:", city)"""
+print("City:", city)
 
 #swapping variables
-"""a = 10
+a = 10
 b = 20
 print("Before Swapping:", a,b)
 a,b = b,a
-print("After Swapping:", a,b)"""
+print("After Swapping:", a,b)
 
 #Area of Rectangle
-"""l = int(input("Enter Length:"))
+l = int(input("Enter Length:"))
 b = int(input("Enter Breadth:"))
-print("Area of Rectangle =", l*b)"""
+print("Area of Rectangle =", l*b)
 
 #Calculate Simple Interest
-"""p = int(input("Enter Principle:"))
+p = int(input("Enter Principle:"))
 r = int(input("Enter Rate of Interest:"))
 t = int(input("Enter Time:"))
 si = p*r*t/100
-print("Simple Interest =", si)"""
+print("Simple Interest =", si)
 
 #Convert celcius to fahrenheit
-"""c = int(input("Celcius ="))
+c = int(input("Celcius ="))
 f = (c * 9/5)+32
-print("Fahrenheit =", f)"""
+print("Fahrenheit =", f)
 
 
 
