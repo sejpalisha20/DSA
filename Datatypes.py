14a8f7de1b6bf391141447ae7b19de46744a2e83
Isha Sejpal <sejpalisha20@gmail.com>
2026-09-22 21:43:00 +0530

    Uncomment print statements for datatype demonstration

--- Datatypes.py
+++ Datatypes.py
@@ -1,5 +1,5 @@
 #Demonstrate datatype
-"""a = 10
+a = 10
 b = 10.5
 c ="Python"
 d = True
@@ -8,34 +8,34 @@ print(a, type(a))
 print(b, type(b))
 print(c, type(c))
 print(d, type(d))
-print(e, type(e))"""
+print(e, type(e))
 
 #Accept two numbers and display their datatype
-"""a = input("Enter First number:")
+a = input("Enter First number:")
 b = input("Enter Second number:")
 print("Type of 1st number:", type(a))
-print("Type of 2nd number:", type(b))"""
+print("Type of 2nd number:", type(b))
 
 #Convert a string number into integer and float
-"""num = "25"
+num = "25"
 i = int(num)
 f = float(num)
 print("Integer:", i)
-print("Float:", f)"""
+print("Float:", f)
 
 #Find the length of the string
-"""a = "Hello World"
-print("Length of string:", len(a))"""
+a = "Hello World"
+print("Length of string:", len(a))
 
 #Create a list, tuple, set and dictionary and display their types
-"""List = [1,2,3,4,5,6]
+List = [1,2,3,4,5,6]
 Tuple = (1,2,3,4,5,6)
 Set = {1,2,3,4,5,6}
 Dictionary = {"name": "Isha", "age":20}
 print(type(List))
 print(type(Tuple))
 print(type(Set))
-print(type(Dictionary))"""
+print(type(Dictionary))
 
 
 
